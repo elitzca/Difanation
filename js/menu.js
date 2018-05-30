dec17c3528e6cc21f662ddcfa74ace587b752993
elitzca <elitzca0raycheva@gmail.com>
2018-05-30 16:07:10 +0200

    newer version of the mobile and desktop version, nav working on desktop as well

--- js/menu.js
+++ js/menu.js
@@ -2,3 +2,13 @@ function toggleMenu() {
     //console.log("i work");
     document.getElementById("menu-dropdown").classList.toggle('active')
 }
+
+document.addEventListener('click', closeMenu)
+
+function closeMenu (e) {
+    //console.log(e.target.classList);
+    if (!e.target.classList.contains("toggle-btn") && !e.target.classList.contains("span-lines") ){
+        //console.log("success");
+        document.getElementById("menu-dropdown").classList.toggle('active')
+    }
+}
