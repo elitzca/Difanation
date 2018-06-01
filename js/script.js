9a214b3653e89eeb0060be4ad673829fa125319a
elitzca <elitzca0raycheva@gmail.com>
2018-06-01 19:15:03 +0200

    video, service section with json in progress, better nav

--- js/script.js
+++ js/script.js
@@ -17,3 +17,38 @@ $(document).ready(function() {
         e.preventDefault();
     });
 });
+
+function fetchServices () {
+    fetch("http://elitzca.eu/kea/wordpress/wp-json/wp/v2/posts")
+        .then(e => e.json())
+        .then(showServices)
+}
+
+function showServices(data){
+    //console.log(data);
+    data.forEach(showSingleService)
+}
+
+function showSingleService (aService) {
+    //console.log(aService);
+    let template = document.querySelector("#svtemp").content;
+    let clone = template.cloneNode(true);
+
+    clone.querySelector("h1").textContent = aService.title.rendered;
+
+    clone.querySelector(".description").innerHTML = aService.content.rendered;
+
+    //clone.querySelector("#background-image").innerHTML
+
+    let servicelist = document.querySelector("#servicelist");
+    servicelist.appendChild(clone);
+}
+
+fetchServices();
+
+
+
+
+
+
+
