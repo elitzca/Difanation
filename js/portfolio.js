2ae83840634a4bc8416f34d1209262395431de9a
Libertadoo <linda.vorobeja@inbox.lv>
2018-06-02 02:24:08 +0200

    works with reloading :D

--- js/portfolio.js
+++ js/portfolio.js
@@ -4,4 +4,11 @@ let hidden = document.querySelector(".hide");
 arrow.addEventListener('click', showMore);
 function showMore(){
 hidden.classList.remove("hide");
+	arrow.classList.add("rotate");
+	arrow.addEventListener('click', showLess);
+	function showLess(){
+		hidden.classList.add("hide");
+		arrow.classList.remove("rotate");
+		location.reload();
+	}
 }
